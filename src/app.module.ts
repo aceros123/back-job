303c63759b366c3dd7657d9d9db7eab04adfd018
aceros123 <57160341+aceros123@users.noreply.github.com>
2024-10-21 21:41:52 -0500

    creacion de componentes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { WorkspacesModule } from './workspaces/workspaces.module';
+import { ReservationModule } from './reservation/reservation.module';
+import { MongooseModule } from 'mongoose';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://DANIEL:123456@localhost:27017/'),AuthModule, UsersModule, WorkspacesModule, ReservationModule],
   controllers: [AppController],
   providers: [AppService],
 })
